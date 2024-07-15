8a0014f6c1061252cd16c55ed4f67769086c0072
Almaz Murtazin <murtazin2021@inbox.ru>
2024-07-15 13:02:44 +0500

    data loss solution

--- src/main/java/org/example/wallet/repository/WalletRepository.java
+++ src/main/java/org/example/wallet/repository/WalletRepository.java
@@ -1,10 +1,19 @@
 package org.example.wallet.repository;
 
+import jakarta.persistence.LockModeType;
 import org.example.wallet.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 import java.util.UUID;
+
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT w FROM Wallet w WHERE w.id = :walletId")
+    Optional<Wallet> findByForUpdate(@Param("walletId") UUID walletTd);
 }
